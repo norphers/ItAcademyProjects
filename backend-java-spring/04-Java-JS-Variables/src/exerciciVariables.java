cc1cc9278b73fdf6a2ec6177ee4396eb82be5ec7
ocanades <ocanades@gmail.com>
2020-08-30 13:37:20 +0200

    refactoring exercise 1 Java

--- backend-java-spring/04-Java-JS-Variables/src/exerciciVariables.java
+++ backend-java-spring/04-Java-JS-Variables/src/exerciciVariables.java
@@ -1,97 +1,87 @@
 import java.util.*;
 
-public class exerciciVariables {
-
-	public static void main(String[] args) {
+public class exerciciVariables 
+{
+	public static void main(String[] args) 
+	{	
+		// Exercise 1.1
 		
-		System.out.println("Step 1" + "\n");
-
-        String name="Oriol";
-        String surname1="Canadés";
-        String surname2="Díez";
+        String firstName="Oriol";
+        String lastName1="Canadés";
+        String lastName2="Díez";
         int day=26;
         int month=2;
         int year=2020;
-        System.out.print(surname1 + " " + surname2 + ", " + name + "\n");
+        System.out.print(lastName1 + " " + lastName2 + ", " + firstName + "\n");
         System.out.print(day + "/" + month + "/" + year);
 
-       
+        // Exercise 1.2
         
-        System.out.println("Step 2" + "\n");
-
         final int constantLeapYear=1948;
 
         int [] arrayLeapYears = new int[1000];
+        
         for (int i=4; i<2024; i+=4)
+        {
             if (i%400==0 || i%4==0 || i/100!=0) {
                 System.out.println(i);
             }
-
-        Scanner entryYear = new Scanner(System.in);
-        System.out.println("Introduce a year, please");
-
-        int birthDate=entryYear.nextInt();
-        int resultLYCount=(birthDate-constantLeapYear)/4;
-
-        System.out.println("Between 1948 and " + birthDate + " there are " + resultLYCount + " leap years.");
-
+        }
         
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Entry your birth year: ");
+
+        int birthYear=sc.nextInt();
         
-        System.out.println("Step 3" + "\n");
+        int resultLYCount=(birthYear-constantLeapYear)/4;
 
-        Scanner entryBirthYear = new Scanner(System.in);
-
+        System.out.println("Between 1948 to " + birthYear + ", there are " + resultLYCount + " leap years.");
+        
+        sc.close();
+        
+        // Exercise 1.3
+        
+        Scanner sc2 = new Scanner(System.in);
+        
         System.out.println("Introduce your birth year, please.");
 
-        int birthYearDate = entryBirthYear.nextInt();
+        int birthYearDate = sc2.nextInt();
 
         int leapYear = 1948;
 
-        for (int i=leapYear; i<birthYearDate; i+=4)
+        for (int i=leapYear; i<birthYearDate; i+=4) {
             System.out.println(i);
-
-        String booLeapYearTrue="It's a leap year";
+        }
         
-        String booLeapYearFalse="It isn't a leap year";
-
-        boolean booLeapYear=false;
-
         if (birthYearDate%400==0 || birthYearDate%4==0 && birthYearDate/100!=0) {
-            booLeapYear=true;
-            System.out.println(booLeapYearTrue);
+            System.out.println("It's a leap year");
+        } else {
+            System.out.println("It isn't a leap year");
         }
 
-        else {
-            System.out.println(booLeapYearFalse);
-        }
+        sc2.close();
+        
+        // Exercise 1.4
 
+        Scanner sc3 = new Scanner(System.in);
         
-        
-        
-        System.out.println("Step 4" + "\n");
-
-        Scanner entryName = new Scanner(System.in);
         System.out.println("Introduce your name, please.");
-        String yourName=entryName.nextLine();
+        String yourName = sc3.nextLine();
 
-        Scanner entrySurname = new Scanner(System.in);
         System.out.println("Now, introduce your surname, please.");
-        String yourSurname=entrySurname.nextLine();
+        String yourSurname = sc3.nextLine();
 
         String completeName = yourName + " " + yourSurname; // complete variable name
 
-        Scanner entryDay = new Scanner(System.in);
         System.out.println("Introduce your day birth, please.");
-        int dayBirth=entryDay.nextInt();
+        int dayBirth = sc3.nextInt();
 
-        Scanner entryMonth = new Scanner(System.in);
         System.out.println("Now, introduce your month birth, please.");
-        int monthBirth=entryMonth.nextInt();
+        int monthBirth = sc3.nextInt();
 
-        Scanner entryYear2 = new Scanner(System.in);
         System.out.println("Finally, introduce your year birth, please.");
-        int yearBirth=entryYear2.nextInt();
+        int yearBirth = sc3.nextInt();
 
         String completeDate = Integer.toString(monthBirth) + "/" + Integer.toString(dayBirth) + "/" + Integer.toString(yearBirth); 
 
@@ -102,7 +92,9 @@ public class exerciciVariables {
         else {
             System.out.println("Your name is " + completeName + "\n" + "You were born " + completeDate + " and " + yearBirth + " was a leap year.");
         }
-
+        
+        sc3.close();
+        
 	}
 
 }
